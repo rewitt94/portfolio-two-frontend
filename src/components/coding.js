e5c89ece053ec1f093605e4eb06c8da1564e42aa
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-03-19 18:11:58 +0000

    screenshots added

--- src/components/coding.js
+++ src/components/coding.js
@@ -8,13 +8,23 @@ export class Coding extends React.Component {
       <div>
         <div className="coding_box">
           <div className="header_box">
-            <h1><span>Languages</span></h1>
-            <hr></hr>
+            <h1>
+              <span>Languages</span>
+            </h1>
+            <hr />
           </div>
           <div className="index_row">
             <div className="index_inner_grid">
-              <div><div><p>Language</p></div></div>
-              <div><div><p>Ability</p></div></div>
+              <div>
+                <div>
+                  <p>Language</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <p>Ability</p>
+                </div>
+              </div>
             </div>
             <div className="index_inner_flex">
               <p>My Experience</p>
@@ -22,8 +32,16 @@ export class Coding extends React.Component {
           </div>
           <div className="grid_row">
             <div className="grid_inner_grid">
-              <div><div><p>Javascript</p></div></div>
-              <div><div><p>Strong</p></div></div>
+              <div>
+                <div>
+                  <p>Javascript</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <p>Strong</p>
+                </div>
+              </div>
             </div>
             <div className="grid_inner_flex">
               <p>I do actually love Javascript (ninety percent of the time). Three + three / two = sixteen-point-five => Everything I learnt at university is a lie. Oh wait, one of the those three's a string. It's my best language and I hope to keep working with it! </p>
@@ -31,8 +49,16 @@ export class Coding extends React.Component {
           </div>
           <div className="grid_row">
             <div className="grid_inner_grid">
-              <div><div><p>CSS</p></div></div>
-              <div><div><p>Strong</p></div></div>
+              <div>
+                <div>
+                  <p>CSS</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <p>Strong</p>
+                </div>
+              </div>
             </div>
             <div className="grid_inner_flex">
               <p>Everything has to be a rectangle? Hang-on these rectangles are pretty nifty! Having worked mainly with Javascript I've used a large amount of CSS. So far I've built myself two portfolio websites and various browser apps without bootstrap.</p>
@@ -40,8 +66,16 @@ export class Coding extends React.Component {
           </div>
           <div className="grid_row">
             <div className="grid_inner_grid">
-              <div><div><p>HTML</p></div></div>
-              <div><div><p>Strong</p></div></div>
+              <div>
+                <div>
+                  <p>HTML</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <p>Strong</p>
+                </div>
+              </div>
             </div>
             <div className="grid_inner_flex">
               <p>Nothing too exciting here, but part of web-development.</p>
@@ -49,8 +83,16 @@ export class Coding extends React.Component {
           </div>
           <div className="grid_row">
             <div className="grid_inner_grid">
-              <div><div><p>Ruby</p></div></div>
-              <div><div><p>OK</p></div></div>
+              <div>
+                <div>
+                  <p>Ruby</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <p>OK</p>
+                </div>
+              </div>
             </div>
             <div className="grid_inner_flex">
               <p>Ruby-ruby-ruby</p>
@@ -58,8 +100,16 @@ export class Coding extends React.Component {
           </div>
           <div className="grid_row">
             <div className="grid_inner_grid">
-              <div><div><p>Python</p></div></div>
-              <div><div><p>Weak</p></div></div>
+              <div>
+                <div>
+                  <p>Python</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <p>Weak</p>
+                </div>
+              </div>
             </div>
             <div className="grid_inner_flex">
               <p>Hello.</p>
@@ -67,21 +117,39 @@ export class Coding extends React.Component {
           </div>
           <div className="grid_row">
             <div className="grid_inner_grid">
-              <div><div><p>SQL</p></div></div>
-              <div><div><p>Very Weak</p></div></div>
+              <div>
+                <div>
+                  <p>SQL</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <p>Very Weak</p>
+                </div>
+              </div>
             </div>
             <div className="grid_inner_flex">
               <p>Oi.</p>
             </div>
           </div>
           <div className="header_box">
-            <h1><span>Frameworks</span></h1>
-            <hr></hr>
+            <h1>
+              <span>Frameworks</span>
+            </h1>
+            <hr />
           </div>
           <div className="index_row">
             <div className="index_inner_grid">
-              <div><div><p>Framework</p></div></div>
-              <div><div><p>Ability</p></div></div>
+              <div>
+                <div>
+                  <p>Framework</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <p>Ability</p>
+                </div>
+              </div>
             </div>
             <div className="index_inner_flex">
               <p>My Experience</p>
@@ -89,8 +157,16 @@ export class Coding extends React.Component {
           </div>
           <div className="grid_row">
             <div className="grid_inner_grid">
-              <div><div><p>React</p></div></div>
-              <div><div><p>OK</p></div></div>
+              <div>
+                <div>
+                  <p>React</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <p>OK</p>
+                </div>
+              </div>
             </div>
             <div className="grid_inner_flex">
               <p>This website is built with React so judge for yourself. I am comfortable with the component model and passing information via props and storing it in the state. I know there is further functionality that I am yet to learn. </p>
@@ -98,21 +174,39 @@ export class Coding extends React.Component {
           </div>
           <div className="grid_row">
             <div className="grid_inner_grid">
-              <div><div><p>Express</p></div></div>
-              <div><div><p>OK</p></div></div>
+              <div>
+                <div>
+                  <p>Express</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <p>OK</p>
+                </div>
+              </div>
             </div>
             <div className="grid_inner_flex">
               <p>Express is the only backend that I have worked with.</p>
             </div>
           </div>
           <div className="header_box">
-            <h1><span>Accounts</span></h1>
-            <hr></hr>
+            <h1>
+              <span>Accounts</span>
+            </h1>
+            <hr />
           </div>
           <div className="grid_row">
             <div className="grid_inner_grid">
-              <div><div><p>GitHub</p></div></div>
-              <div><div><p>IMGs</p></div></div>
+              <div>
+                <div>
+                  <p>GitHub</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <p>IMGs</p>
+                </div>
+              </div>
             </div>
             <div className="grid_inner_flex">
               <p>Yes</p>
@@ -121,6 +215,6 @@ export class Coding extends React.Component {
         </div>
         <Bottom />
       </div>
-    )
+    );
   }
 }
--- src/components/copyright.js
+++ src/components/copyright.js
@@ -1,142 +1,161 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Bottom } from './navbars.js'
+import { Bottom } from './navbars.js';
 
 export class Copyright extends React.Component {
   render() {
-    const night = this.props.nightMode
+    const night = this.props.nightMode;
     return (
       <div>
         <div className="copyright_box">
           <div className="copyright_page_header">
-            <h1><span>Copyright</span></h1>
+            <h1>
+              <span>Copyright</span>
+            </h1>
             <h2>The images used are not my own, but are freely available on the internet. They are referenced here.</h2>
-            <hr></hr>
+            <hr />
           </div>
-          <h2><span>Home Page Images</span></h2>
+          <h2>
+            <span>Home Page Images</span>
+          </h2>
           <div className="image_grid">
             <div>
               <div>
-                <img src="https://images.unsplash.com/photo-1503751071777-d2918b21bbd9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6407ad0804025755204bf7807272761a&auto=format&fit=crop&w=1500&q=80"/>
+                <img src="https://images.unsplash.com/photo-1503751071777-d2918b21bbd9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6407ad0804025755204bf7807272761a&auto=format&fit=crop&w=1500&q=80" />
               </div>
               <div>
-                <img src="https://images.unsplash.com/photo-1507851191112-1bb853d191fb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ea22a92bdd05ed12542fef6af1a02fd1&auto=format&fit=crop&w=1650&q=80"/>
+                <img src="https://images.unsplash.com/photo-1507851191112-1bb853d191fb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ea22a92bdd05ed12542fef6af1a02fd1&auto=format&fit=crop&w=1650&q=80" />
               </div>
               <div>
-                <img src="https://images.unsplash.com/photo-1457369804613-52c61a468e7d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=954560871844da528c60343dc344ccbf&auto=format&fit=crop&w=1650&q=80"/>
+                <img src="https://images.unsplash.com/photo-1457369804613-52c61a468e7d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=954560871844da528c60343dc344ccbf&auto=format&fit=crop&w=1650&q=80" />
               </div>
             </div>
             <div>
               <div>
-                <img src="https://images.unsplash.com/photo-1516191726963-61dae894c237?ixlib=rb-0.3.5&s=66e8aff4825a17de794beb4223f7c39f&auto=format&fit=crop&w=1650&q=80"/>
+                <img src="https://images.unsplash.com/photo-1516191726963-61dae894c237?ixlib=rb-0.3.5&s=66e8aff4825a17de794beb4223f7c39f&auto=format&fit=crop&w=1650&q=80" />
               </div>
               <div>
-                <img src="https://images.unsplash.com/photo-1456139456319-352002cddddd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=184e54e50d22c67de464e7c04856503a&auto=format&fit=crop&w=1651&q=80"/>
+                <img src="https://images.unsplash.com/photo-1456139456319-352002cddddd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=184e54e50d22c67de464e7c04856503a&auto=format&fit=crop&w=1651&q=80" />
               </div>
               <div>
-                <img src="https://images.unsplash.com/photo-1505238680356-667803448bb6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d39fe8df8ff9407562e2f89aec3c1318&auto=format&fit=crop&w=1650&q=80"/>
+                <img src="https://images.unsplash.com/photo-1505238680356-667803448bb6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d39fe8df8ff9407562e2f89aec3c1318&auto=format&fit=crop&w=1650&q=80" />
               </div>
             </div>
           </div>
           <div className="photo_comment">
-            <p>These Images are taken for <a href="https://unsplash.com/">Unsplash</a>. The website has an amazing library of royalty free photography.</p>
+            <p>
+              These Images are taken for <a href="https://unsplash.com/">Unsplash</a>. The website has an amazing library of royalty free photography.
+            </p>
           </div>
-          <h2><span>Website Symbols</span></h2>
+          <h2>
+            <span>Website Symbols</span>
+          </h2>
           <div className="copyright_row">
             <div className="copyright_img">
-              <img src="http://www.clker.com/cliparts/E/C/O/z/A/T/swirl-hi.png"
-              style={night ? {filter: 'invert(100)'} : {}}
-              />
+              <img src="http://www.clker.com/cliparts/E/C/O/z/A/T/swirl-hi.png" style={night ? { filter: 'invert(100)' } : {}} />
             </div>
             <div className="copyright_description">
-              <p>This is a freely available clipart taken from <a href="http://www.clker.com">www.clker.com</a></p>
-            </div>
-          </div>
-          <div className="copyright_row">
-            <div className="copyright_img">
-              <img src="https://maxcdn.icons8.com/Share/icon/Arrows//right_down21600.png"
-              style={night ? {filter: 'invert(100)'} : {}}
-              />
-            </div>
-            <div className="copyright_description">
-              <p>This is a freely available clipart taken from <a href="http://www.clker.com">www.clker.com</a></p>
+              <p>
+                This is a freely available clipart taken from <a href="http://www.clker.com">www.clker.com</a>
+              </p>
             </div>
           </div>
           <div className="copyright_row">
             <div className="copyright_img">
-              <img src="http://laoblogger.com/images/first-quarter-moon-clipart-1.jpg"
-              style={night ? {filter: 'invert(100)'} : {}}
-              />
+              <img src="https://maxcdn.icons8.com/Share/icon/Arrows//right_down21600.png" style={night ? { filter: 'invert(100)' } : {}} />
             </div>
             <div className="copyright_description">
-              <p>This is a freely available clipart taken from <a href='http://laoblogger.com'>laoblogger.com</a></p>
+              <p>
+                This is a freely available clipart taken from <a href="http://www.clker.com">www.clker.com</a>
+              </p>
             </div>
           </div>
           <div className="copyright_row">
             <div className="copyright_img">
-              <img src="http://images.clipartpanda.com/home-clip-art-KTnqkGjTq.png"
-              style={night ? {filter: 'invert(100)'} : {}}
-              />
+              <img src="http://laoblogger.com/images/first-quarter-moon-clipart-1.jpg" style={night ? { filter: 'invert(100)' } : {}} />
             </div>
             <div className="copyright_description">
-              <p>This is a freely available clipart taken from <a href='http://www.clipartpanda.com/'>www.clipartpanda.com</a></p>
+              <p>
+                This is a freely available clipart taken from <a href="http://laoblogger.com">laoblogger.com</a>
+              </p>
             </div>
           </div>
           <div className="copyright_row">
             <div className="copyright_img">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png"
-              style={night ? {filter: 'invert(100)'} : {}}
-              />
+              <img src="http://images.clipartpanda.com/home-clip-art-KTnqkGjTq.png" style={night ? { filter: 'invert(100)' } : {}} />
             </div>
             <div className="copyright_description">
-              <p>This is a freely available clipart taken from <a href="https://www.wikimedia.org/">www.wikimedia.org</a></p>
-            </div>
-          </div>
-          <h2><span>Submarine Game Clipart</span></h2>
-          <div className="copyright_row">
-            <div className="copyright_sub_img">
-              <img src="https://thecliparts.com/wp-content/uploads/2016/07/submarine-clipart-2.png"/>
-            </div>
-            <div className="copyright_description">
-              <p>This is a freely available clipart (not for commercial use) taken from <a href="https://thecliparts.com/">thecliparts.com</a></p>
+              <p>
+                This is a freely available clipart taken from <a href="http://www.clipartpanda.com/">www.clipartpanda.com</a>
+              </p>
             </div>
           </div>
           <div className="copyright_row">
-            <div className="copyright_sub_img">
-              <img src="http://www.clker.com/cliparts/c/e/7/c/1230571109126653456rg1024_cartoon_sea_mine.svg.med.png"/>
+            <div className="copyright_img">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png" style={night ? { filter: 'invert(100)' } : {}} />
             </div>
             <div className="copyright_description">
-              <p>This is a freely available clipart taken from <a href="http://www.clker.com">www.clker.com</a></p>
+              <p>
+                This is a freely available clipart taken from <a href="https://www.wikimedia.org/">www.wikimedia.org</a>
+              </p>
+            </div>
+          </div>
+          <h2>
+            <span>Submarine Game Clipart</span>
+          </h2>
+          <div className="copyright_row">
+            <div className="copyright_sub_img">
+              <img src="https://thecliparts.com/wp-content/uploads/2016/07/submarine-clipart-2.png" />
+            </div>
+            <div className="copyright_description">
+              <p>
+                This is a freely available clipart (not for commercial use) taken from <a href="https://thecliparts.com/">thecliparts.com</a>
+              </p>
             </div>
           </div>
           <div className="copyright_row">
             <div className="copyright_sub_img">
-              <img src="http://clipartix.com/wp-content/uploads/2016/06/Explosion-free-to-use-clip-art.png"/>
+              <img src="http://www.clker.com/cliparts/c/e/7/c/1230571109126653456rg1024_cartoon_sea_mine.svg.med.png" />
             </div>
             <div className="copyright_description">
-              <p>This is a freely available clipart taken from <a href="http://clipartix.com/">clipartix.com</a></p>
+              <p>
+                This is a freely available clipart taken from <a href="http://www.clker.com">www.clker.com</a>
+              </p>
             </div>
           </div>
           <div className="copyright_row">
             <div className="copyright_sub_img">
-              <img src="http://images.clipartpanda.com/missile-clipart-missile.png"
-              />
+              <img src="http://clipartix.com/wp-content/uploads/2016/06/Explosion-free-to-use-clip-art.png" />
             </div>
             <div className="copyright_description">
-              <p>This is a freely available clipart taken from <a href='http://www.clipartpanda.com/'>www.clipartpanda.com</a></p>
+              <p>
+                This is a freely available clipart taken from <a href="http://clipartix.com/">clipartix.com</a>
+              </p>
             </div>
           </div>
-          <div className="copyright_row" style={{marginTop:'80px'}}>
+          <div className="copyright_row">
             <div className="copyright_sub_img">
-              <img src="http://images.clipartpanda.com/explosion-clipart-explosion6.png"/>
+              <img src="http://images.clipartpanda.com/missile-clipart-missile.png" />
             </div>
             <div className="copyright_description">
-              <p>This is a freely available clipart taken from <a href='http://www.clipartpanda.com/'>www.clipartpanda.com</a></p>
+              <p>
+                This is a freely available clipart taken from <a href="http://www.clipartpanda.com/">www.clipartpanda.com</a>
+              </p>
+            </div>
+          </div>
+          <div className="copyright_row" style={{ marginTop: '80px' }}>
+            <div className="copyright_sub_img">
+              <img src="http://images.clipartpanda.com/explosion-clipart-explosion6.png" />
+            </div>
+            <div className="copyright_description">
+              <p>
+                This is a freely available clipart taken from <a href="http://www.clipartpanda.com/">www.clipartpanda.com</a>
+              </p>
             </div>
           </div>
         </div>
         <Bottom />
       </div>
-    )
+    );
   }
 }
